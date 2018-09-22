381e2de95f803bb5c5ee207a6f0556f134d9acaf
linuxembe <linuxembe@126.com>
2018-09-22 08:21:59 -0700

    declare

--- shell.sh
+++ shell.sh
@@ -1,3 +1,25 @@
 echo "This is a test shell"
 
 echo "this is the second shell"
+
+declare -A ass_array
+
+ass_array=([index1]=vall [index2]=va12 )
+
+ass_array[index2]=val2
+
+declare -A fruits_value
+fruits_value=([apple]='100 dollars' [orange]='150 dollars')
+
+echo "Apple costs ${fruit_value[apple]}"
+
+echo ${!ass_array[*]}
+
+echo ${!ass_array[@]}
+
+echo ${!fruit_value[*]}
+
+alias new_command='command sequence'
+
+alias install='sudo apt-get install'
+
